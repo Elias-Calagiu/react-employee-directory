d4fab64198b8b30c240e13b4f7064518a2928248
Elias Calagiu <eliasc81@gmail.com>
2021-02-17 21:37:41 -0800

    added API search functionality and event handlers to Home.js added code to render card data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react'
 import Card from "../components/Card"
-
+import API from "../utils/API"
+import EmployeeInfo from "../components/EmployeeInfo"
 
 export default class Home extends Component {
-state = {
-    employee: [],
-    displayed: [],
-    search:""
-}
+    state = {
+        employee: [],
+        displayed: [],
+        search: ""
+    }
 
-  
-  componentDidMount() {
-    this.searchEmployees();
-  }
 
-  
+    componentDidMount() {
+        this.searchEmployees();
+    }
+
+    searchEmployees = BASEURL => {
+        API.search(BASEURL)
+            .then(res => this.setState({ result: res.data }))
+            .catch(err => console.log(err));
+    };
+
+    handleSearch = event => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    // When the form is submitted, search the OMDB API for the value of `this.state.search`
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.searchEmployees(this.state.search);
+    };
     render() {
         return (
             <div>
                 <h1>Employee Directory</h1>
                 <div>
-               
-            </div>
+                    <label>Search</label>
+                    <input onChange={this.handleSearch} type="text" name="search" value={this.state.search} />
+                    {this.state.employee.map(employee => <Card data={employee} onChange={this.handleSearch} />)}
+                </div>
             </div>
         )
     }
