f9404d0ff7c1238cc1dbca2c8ae65e24290bb063
Elias Calagiu <eliasc81@gmail.com>
2021-02-17 20:55:01 -0800

    added initial state to Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import Card from "../components/Card"
 export default class Home extends Component {
+state = {
+    employee: [],
+    displayed: [],
+    search:""
+}
+
     render() {
         return (
             <div>
-                <h1>Home Page</h1>
+                <h1>Employee Directory</h1>
             </div>
         )
     }
